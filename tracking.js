ea2f3d39bfbef4d9bd07ba4d74ebffef4b15a7be
Thanh Dung <87826003+yuuyung99@users.noreply.github.com>
2026-08-17 15:02:55 +0700

    Add files via upload

--- tracking.js
+++ tracking.js
@@ -1,1 +1,28 @@
 // JavaScript source code
+/* =========================================
+TRACKING CLICK
+========================================= */
+
+document.addEventListener('click', function (e) {
+
+    const button =
+        e.target.closest('.track-click');
+
+    if (!button) return;
+
+    const eventName =
+        button.dataset.event;
+
+    if (!eventName) return;
+
+    if (window.dataLayer) {
+
+        window.dataLayer.push({
+
+            event: eventName
+
+        });
+
+    }
+
+});
